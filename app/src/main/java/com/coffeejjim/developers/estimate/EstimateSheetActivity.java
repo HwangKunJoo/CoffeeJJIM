91cb18bc349c3b34a9af28fad018b87c857d27e6
hwangkunjoo <hwangkunjoo@gmail.com>
2016-08-24 23:36:49 +0900

    EstimateSheetFragment 추가

--- app/src/main/java/com/coffeejjim/developers/estimate/EstimateSheetActivity.java
+++ app/src/main/java/com/coffeejjim/developers/estimate/EstimateSheetActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.coffeejjim.developers.R;
+import com.coffeejjim.developers.login.LoginFragment;
 
 public class EstimateSheetActivity extends AppCompatActivity {
 
@@ -11,5 +12,11 @@ public class EstimateSheetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estimate_sheet);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new EstimateSheetFragment())
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/coffeejjim/developers/estimate/EstimateSheetFragment.java
+++ app/src/main/java/com/coffeejjim/developers/estimate/EstimateSheetFragment.java
@@ -1,7 +1,14 @@
 package com.coffeejjim.developers.estimate;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.coffeejjim.developers.R;
+
+import butterknife.ButterKnife;
 
 
 public class EstimateSheetFragment extends Fragment {
@@ -9,6 +16,14 @@ public class EstimateSheetFragment extends Fragment {
 
     public EstimateSheetFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fr_estimate_sheet, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
 }
