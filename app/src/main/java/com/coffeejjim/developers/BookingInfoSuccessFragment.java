c9cb05e3e61bd169e9b68999b2642ebd193b174d
namgil <tompx@naver.com>
2016-08-24 15:34:19 +0900

    sh: Layout modify v5 (includ dialog)

---
+++ app/src/main/java/com/coffeejjim/developers/BookingInfoSuccessFragment.java
@@ -0,0 +1,28 @@
+package com.coffeejjim.developers;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BookingInfoSuccessFragment extends Fragment {
+
+
+    public BookingInfoSuccessFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fr_booking_info_success, container, false);
+    }
+
+}
--- app/src/main/java/com/coffeejjim/developers/EstimatesheetConfirmActivity.java
+++ app/src/main/java/com/coffeejjim/developers/EstimatesheetConfirmActivity.java
@@ -1,7 +1,11 @@
 package com.coffeejjim.developers;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class EstimatesheetConfirmActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class EstimatesheetConfirmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estimatesheet_confirm);
+
+    }
+
+    public void onAlertDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(EstimatesheetConfirmActivity.this);
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+        builder.setTitle("Coffee JJIM");
+        builder.setMessage("견적 요청이 들어왔습니다. 확인하시겠습니까?");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                Toast.makeText(EstimatesheetConfirmActivity.this, "확인", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                Toast.makeText(EstimatesheetConfirmActivity.this, "닫기", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setCancelable(false);
+        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                Toast.makeText(EstimatesheetConfirmActivity.this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
