86245260d5ea34d319954161b069ed29ba630603
hwangkunjoo <hwangkunjoo@gmail.com>
2016-08-25 14:10:54 +0900

    Home 네비완료

--- app/src/main/java/com/coffeejjim/developers/ProviderHomeFragment.java
+++ app/src/main/java/com/coffeejjim/developers/ProviderHomeFragment.java
@@ -16,6 +16,6 @@ public class ProviderHomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fr_provider_hmoe, container, false);
+        return inflater.inflate(R.layout.fr_provider_home, container, false);
     }
 }
