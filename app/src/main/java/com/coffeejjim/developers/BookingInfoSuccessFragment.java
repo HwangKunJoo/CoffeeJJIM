caf2fa33f3aaa1cc4422b6e79c09fbe554da2504
namgil <tompx@naver.com>
2016-08-25 12:35:47 +0900

    fr_booking_info_success 다이얼로그 추가

--- app/src/main/java/com/coffeejjim/developers/BookingInfoSuccessFragment.java
+++ app/src/main/java/com/coffeejjim/developers/BookingInfoSuccessFragment.java
@@ -1,16 +1,29 @@
 package com.coffeejjim.developers;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BookingInfoSuccessFragment extends Fragment {
+public class BookingInfoSuccessFragment extends DialogFragment {
 
 
     public BookingInfoSuccessFragment() {
         // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fr_booking_info_success, container);
+        return view;
     }
 
 }
