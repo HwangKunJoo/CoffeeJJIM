f4642ae346622568459cafd28665354e4d48a113
namgil <tompx@naver.com>
2016-08-26 13:41:38 +0900

    로그아웃 메세지 변경

--- app/src/main/java/com/coffeejjim/developers/ExtraFunctionsActivity.java
+++ app/src/main/java/com/coffeejjim/developers/ExtraFunctionsActivity.java
@@ -1,11 +1,15 @@
 package com.coffeejjim.developers;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.coffeejjim.developers.login.LoginActivity;
 
 public class ExtraFunctionsActivity extends AppCompatActivity {
 
@@ -75,6 +79,22 @@ public class ExtraFunctionsActivity extends AppCompatActivity {
         builder.setTitle("Coffee JJIM");
         builder.setMessage("로그아웃 되었습니다.");
         builder.setCancelable(true);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent LoginIntent = new Intent(ExtraFunctionsActivity.this, LoginActivity.class);
+                startActivity(LoginIntent);
+                finish();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(ExtraFunctionsActivity.this,"취소 되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setCancelable(false);
         dialog = builder.create();
         dialog.show();
     }
