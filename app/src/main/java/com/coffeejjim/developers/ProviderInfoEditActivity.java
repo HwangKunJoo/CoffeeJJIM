ab02c1d7d60423742af9d2c5e2cdc7b6755d5ac2
namgil <tompx@naver.com>
2016-09-02 13:14:14 +0900

    레이아웃 3차 수정 완료

--- app/src/main/java/com/coffeejjim/developers/ProviderInfoEditActivity.java
+++ app/src/main/java/com/coffeejjim/developers/ProviderInfoEditActivity.java
@@ -2,6 +2,10 @@ package com.coffeejjim.developers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ProviderInfoEditActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class ProviderInfoEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_info_edit);
+
+        String[] str = getResources().getStringArray(R.array.emailArray);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,str);
+        Spinner spi = (Spinner)findViewById(R.id.activity_provider_info_edit_email_spinner);
+        spi.setAdapter(adapter);
+        spi.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                }
+        );
     }
 }
