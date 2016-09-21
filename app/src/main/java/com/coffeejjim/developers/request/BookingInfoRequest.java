5a8d689a6e5d0f6096d7b2d0f7f2f6e1b1a2a2c5
hwangkunjoo <hwangkunjoo@gmail.com>
2016-09-21 19:29:38 +0900

    즐겨찾기 고침

---
+++ app/src/main/java/com/coffeejjim/developers/request/BookingInfoRequest.java
@@ -0,0 +1,47 @@
+package com.coffeejjim.developers.request;
+
+import android.content.Context;
+
+import com.coffeejjim.developers.data.NetworkResult;
+import com.coffeejjim.developers.data.Proposal;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+
+import okhttp3.HttpUrl;
+import okhttp3.Request;
+
+/**
+ * Created by Tacademy on 2016-09-21.
+ */
+
+public class BookingInfoRequest extends AbstractRequest<NetworkResult<Proposal>> {
+
+    Request request;
+
+    public BookingInfoRequest(Context context, int estimateId, int proposalId) {
+        HttpUrl url = getBaseUrlHttpsBuilder()
+                .addPathSegment("estimates")
+                .addPathSegment("" + estimateId)
+                .addPathSegment("proposalId")
+                .addPathSegment("" + proposalId)
+                .build();
+
+        request = new Request.Builder()
+                .url(url)
+                .tag(context)
+                .build();
+    }
+
+    @Override
+    protected Type getType() {
+        return new TypeToken<NetworkResult<Proposal>>() {
+        }.getType();
+    }
+
+    @Override
+    public Request getRequest() {
+        return request;
+    }
+
+}
