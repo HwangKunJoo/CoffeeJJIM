a78e07d34a1e8e0c9c39c3de2bfc9d5f6822da83
hwangkunjoo <hwangkunjoo@gmail.com>
2016-09-22 15:00:37 +0900

    화면 넘기기 수정중

--- app/src/main/java/com/coffeejjim/developers/estimate/EstimateSheetFragment.java
+++ app/src/main/java/com/coffeejjim/developers/estimate/EstimateSheetFragment.java
@@ -10,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -60,13 +61,18 @@ public class EstimateSheetFragment extends Fragment implements GoogleApiClient.O
     ImageButton btn_socket;
     @BindView(R.id.estimate_sheet_days_btn)
     ImageButton btn_days;
+    @BindView(R.id.estimate_sheet_auction_time)
+    EditText auctionTimeView;
 
     GoogleApiClient mApiClient;
 
     String longitude;
     String latitude;
 
-    public static final int ESTIMATE_CALL = 10;
+    int optionWifi = 0;
+    int optionParking = 0;
+    int optionSocket = 0;
+    int optionDays = 0;
 
     public EstimateSheetFragment() {
         // Required empty public constructor
@@ -157,8 +163,8 @@ public class EstimateSheetFragment extends Fragment implements GoogleApiClient.O
         TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
-                String reservationTime = String.format("%d:%d", hourOfDay, minute);
-                timeView.setText(reservationTime);
+                String reservationTime = String.format("%d:%02d", hourOfDay, minute);
+                timeView.setText(reservationTime + ":00");
             }
         }, hour, minute, false);
 
@@ -178,19 +184,93 @@ public class EstimateSheetFragment extends Fragment implements GoogleApiClient.O
                 int year = date.getYear();
                 int month = date.getMonth() + 1;
                 int day = date.getDay();
-                String reservationDate = String.format("%d-%d-%d", year, month, day);
+                String reservationDate = String.format("%d-%02d-%02d", year, month, day);
                 dateView.setText(reservationDate);
             }
         });
     }
 
-    //옵션들 선택 이미지 바꾸기, 위치 변환,
+    @OnClick(R.id.estimate_sheet_wifi_btn)
+    public void onWifiClick() {
+        if (optionWifi == 1) {
+            optionWifi = 0;
+            btn_wifi.setImageResource(R.drawable.options_wifi_off_120);
+        } else {
+            optionWifi = 1;
+            btn_wifi.setImageResource(R.drawable.options_wifi_on_120);
+        }
+    }
+
+    @OnClick(R.id.estimate_sheet_parking_btn)
+    public void onParkingClick() {
+        if (optionParking == 1) {
+            optionParking = 0;
+            btn_parking.setImageResource(R.drawable.options_parking_off_120);
+        } else {
+            optionParking = 1;
+            btn_parking.setImageResource(R.drawable.options_parking_on_120);
+        }
+    }
+
+    @OnClick(R.id.estimate_sheet_days_btn)
+    public void onDaysClick() {
+        if (optionDays == 1) {
+            optionDays = 0;
+            btn_days.setImageResource(R.drawable.options_workingtime_off_120);
+        } else {
+            optionDays = 1;
+            btn_days.setImageResource(R.drawable.options_wprkingtime_on_120);
+        }
+    }
+
+    @OnClick(R.id.estimate_sheet_socket_btn)
+    public void onSocketClick() {
+        if (optionSocket == 1) {
+            optionSocket = 0;
+            btn_socket.setImageResource(R.drawable.options_plag_off_120);
+        } else {
+            optionSocket = 1;
+            btn_socket.setImageResource(R.drawable.options_plag_on_120);
+        }
+    }
+
+
+//    public static boolean isStringInt(String s) {
+//        try {
+//            Integer.parseInt(s);
+//            return true;
+//        } catch (NumberFormatException e) {
+//            return false;
+//        }
+//    }
+
+
     @OnClick(R.id.btn_estimate_sheet_present)
     public void onEstimateCheckDialogButtonClick() {
+        if(TextUtils.isEmpty(dateView.getText().toString())){
+            Toast.makeText(getContext(), "날짜나 시간 입력이 되지 않았습니다. \n 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(peopleView.getText().toString())) {
+            Toast.makeText(getContext(), "예약 인원이 없습니다. \n 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(auctionTimeView.getText().toString())) {
+            Toast.makeText(getContext(), "경매 시간을 입력해주세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+//        if (!isStringInt(peopleView.getText().toString())) {
+//            Toast.makeText(getContext(), "인원 수를 잘못입력하셨습니다. 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
         String location = locationView.getText().toString();
         String reservationTime = dateView.getText().toString() + " " + timeView.getText().toString();
+        int people = Integer.parseInt(peopleView.getText().toString());
+        int auctionTime = Integer.parseInt(auctionTimeView.getText().toString());
 
-        EstimateRequest ERequest = new EstimateRequest(getContext(), 3, 37.477025, 126.963493, "2016-12-12 16:43:23", 1, 0, 0, 0, 1);
+        EstimateRequest ERequest = new EstimateRequest(getContext(), people, 37.477025, 126.963493
+                , reservationTime, optionWifi, optionDays, optionParking, optionSocket, auctionTime);
         NetworkManager.getInstance().getNetworkData(ERequest, new NetworkManager.OnResultListener<NetworkResult<Estimate>>() {
             @Override
             public void onSuccess(NetworkRequest<NetworkResult<Estimate>> request, NetworkResult<Estimate> result) {
